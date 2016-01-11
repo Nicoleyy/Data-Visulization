eb85bc6f6cf130419d93f849318f450727eb4ef3
wang xiu <xiu.wang@sap.com>
2016-01-11 18:47:42 +0800

    start with a http server
    add router to handle request and operate response with non-block
    
    handle post request

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var http = require("http");
+var url = require("url");
 
-function start(){
-    http.createServer(function (request, response) {
-        console.log("Request recevied");
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
-    }).listen(8888);
-    console.log("Server has started");
+function start(route, handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+        route(handle, pathname, response, request);
+      }
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
 }
 
 exports.start = start;
